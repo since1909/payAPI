da899385e8d96c40a7c822fa4308d2365c6b1203
jiwon <since1909@daum.net>
2021-10-18 17:45:38 +0900

    mapper 찾지 못하는 오류 수정

--- src/main/java/com/hw/payAPI/PayApiApplication.java
+++ src/main/java/com/hw/payAPI/PayApiApplication.java
@@ -1,11 +1,30 @@
 package com.hw.payAPI;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
+@MapperScan(value={"com.hw.payAPI.*"})
 public class PayApiApplication {
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
 
+		Resource[] res
+				= new PathMatchingResourcePatternResolver().getResources("classpath:mapper/**/*.xml");
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PayApiApplication.class, args);
 	}
--- src/main/java/com/hw/payAPI/mapper/PayMapper.java
+++ src/main/java/com/hw/payAPI/mapper/PayMapper.java
@@ -6,6 +6,6 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface PayMapper {
-    @Insert("INSERT INTO payments(unique_id, payStr) VALUES(#{unique_id}, #{payStr})")
+    //@Insert("INSERT INTO payments(unique_id, payStr) VALUES(#{unique_id}, #{payStr})")
     int savePayStr(@Param("unique_id") String unique_id , @Param("payStr") String payStr);
 }
