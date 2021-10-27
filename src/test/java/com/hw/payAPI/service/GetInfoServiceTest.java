59aec4603d63b3fb53c3c8dfee46756c019caaad
jiwon <since1909@daum.net>
2021-10-27 16:33:57 +0900

    Refactor : import문 수정

--- src/test/java/com/hw/payAPI/service/GetInfoServiceTest.java
+++ src/test/java/com/hw/payAPI/service/GetInfoServiceTest.java
@@ -7,11 +7,9 @@ import org.apache.commons.codec.DecoderException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
